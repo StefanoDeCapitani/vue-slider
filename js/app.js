04f6c499e59ea2dc5fa5b0991a78951d02b5423c
Stefano De Capitani <stefano.deca@live.com>
2021-10-29 15:43:26 +0200

    Slider done

--- js/app.js
+++ js/app.js
@@ -6,32 +6,46 @@ window.addEventListener("DOMContentLoaded", function () {
     data: {
       imagesList: [
         {
-          image: "img/01.jpg",
+          url: "img/01.jpg",
           title: "Svezia",
           text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
         },
         {
-          image: "img/02.jpg",
+          url: "img/02.jpg",
           title: "Svizzera",
           text: "Lorem ipsum",
         },
         {
-          image: "img/03.jpg",
+          url: "img/03.jpg",
           title: "Gran Bretagna",
           text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
         },
         {
-          image: "img/04.jpg",
+          url: "img/04.jpg",
           title: "Germania",
           text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam",
         },
         {
-          image: "img/05.jpg",
+          url: "img/05.jpg",
           title: "Paradise",
           text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam",
         },
       ],
+      currentImageIndex: 0,
     },
-    methods: {},
+    methods: {
+      showPrevious() {
+        this.currentImageIndex =
+          this.currentImageIndex > 0
+            ? --this.currentImageIndex
+            : this.imagesList.length - 1;
+      },
+      showNext() {
+        this.currentImageIndex =
+          this.currentImageIndex < this.imagesList.length - 1
+            ? ++this.currentImageIndex
+            : 0;
+      },
+    },
   });
 });
